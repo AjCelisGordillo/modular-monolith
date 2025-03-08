a97a03b2d9bc2d534cb3a23bdb82686c70d092bd
Alvaro C. Gordillo <alvaro.gordillo@massygroup.com>
2025-03-08 12:24:02 -0500

    Asynchronous Communication Between Modules

---
+++ Evently.Common.Application/EventBus/IntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Evently.Common.Application.EventBus;
+
+public abstract class IntegrationEvent : IIntegrationEvent
+{
+    protected IntegrationEvent(Guid id, DateTime ocurredOnUtc)
+    {
+        Id = id;
+        OcurredOnUtc = ocurredOnUtc;
+    }
+
+    public Guid Id { get; init; }
+    public DateTime OcurredOnUtc { get; init; }
+}
