88f1bffa543f36cb0d95dcd2b66b86330eb2aaf0
Alvaro C. Gordillo <alvaro.gordillo@massygroup.com>
2025-03-08 11:13:50 -0500

    Duplicating Data Between Modules

--- Evently.Modules.Ticketing.Application/Carts/AddItemToCart/AddItemToCartCommand.cs
+++ Evently.Modules.Ticketing.Application/Carts/AddItemToCart/AddItemToCartCommand.cs
@@ -2,30 +2,23 @@
 using Evently.Common.Domain;
 using Evently.Modules.Events.Domain.TicketTypes;
 using Evently.Modules.Events.PublicApi;
+using Evently.Modules.Ticketing.Domain.Customers;
 using Evently.Modules.Users.Domain.Users;
-using Evently.Modules.Userts.PublicApi;
-using FluentValidation;
 
 namespace Evently.Modules.Ticketing.Application.Carts.AddItemToCart;
 
 public sealed record AddItemToCartCommand(Guid CustomerId, Guid TicketTypeId, decimal Quantity) : ICommand;
 
-internal sealed class AddItemToCartCommandValidator : AbstractValidator<AddItemToCartCommand>
-{
-    public AddItemToCartCommandValidator()
-    {
-        RuleFor(c => c.CustomerId).NotEmpty();
-        RuleFor(c => c.TicketTypeId).NotEmpty();
-        RuleFor(c => c.Quantity).GreaterThan(decimal.Zero);
-    }
-}
-
-internal sealed class AddItemToCartCommandHandler(CartService cartService, IUsersApi usersApi, IEventsApi eventsApi) : ICommandHandler<AddItemToCartCommand>
+internal sealed class AddItemToCartCommandHandler(
+    CartService cartService,
+    ICustomerRepository customerRepository,
+    IEventsApi eventsApi
+) : ICommandHandler<AddItemToCartCommand>
 {
     public async Task<Result> Handle(AddItemToCartCommand request, CancellationToken cancellationToken)
     {
         // 1. Get Customer
-        UserResponse? customer = await usersApi.GetAsync(request.CustomerId, cancellationToken);
+        Customer? customer = await customerRepository.GetAsync(request.CustomerId, cancellationToken);
 
         if (customer is null)
         {
