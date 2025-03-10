fea17fab7679e0aa74037cd88b5c696a216868ee
Alvaro C. Gordillo <alvaro.gordillo@massygroup.com>
2025-03-10 10:07:01 -0500

    User Authentication with keycloak

--- Evently.Modules.Ticketing.Application/Carts/AddItemToCart/AddItemToCartCommand.cs
+++ Evently.Modules.Ticketing.Application/Carts/AddItemToCart/AddItemToCartCommand.cs
@@ -1,50 +1,6 @@
 ﻿using Evently.Common.Application.Messaging;
-using Evently.Common.Domain;
-using Evently.Modules.Events.Domain.TicketTypes;
-using Evently.Modules.Events.PublicApi;
-using Evently.Modules.Ticketing.Domain.Customers;
-using Evently.Modules.Users.Domain.Users;
 
 namespace Evently.Modules.Ticketing.Application.Carts.AddItemToCart;
 
 public sealed record AddItemToCartCommand(Guid CustomerId, Guid TicketTypeId, decimal Quantity) : ICommand;
-
-internal sealed class AddItemToCartCommandHandler(
-    CartService cartService,
-    ICustomerRepository customerRepository,
-    IEventsApi eventsApi
-) : ICommandHandler<AddItemToCartCommand>
-{
-    public async Task<Result> Handle(AddItemToCartCommand request, CancellationToken cancellationToken)
-    {
-        // 1. Get Customer
-        Customer? customer = await customerRepository.GetAsync(request.CustomerId, cancellationToken);
-
-        if (customer is null)
-        {
-            return Result.Failure(UserErrors.NotFound(request.CustomerId));
-        }
-
-        // 2. Get ticket type
-        TicketTypeResponse? ticketType = await eventsApi.GetTicketTypeAsync(request.TicketTypeId, cancellationToken);
-
-        if (ticketType is null)
-        {
-            return Result.Failure(TicketTypeErrors.NotFound(request.TicketTypeId));
-        }
-
-        // 3. Add item to cart
-        var cartItem = new CartItem
-        {
-            TicketTypeId = ticketType.Id,
-            Price = ticketType.Price,
-            Quantity = request.Quantity,
-            Currency = ticketType.Currency
-        };
-
-        await cartService.AddItemAsync(customer.Id, cartItem, cancellationToken);
-
-        return Result.Success();
-    }
-}
 
--- Evently.Modules.Users.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
+++ Evently.Modules.Users.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -1,11 +1,13 @@
 ﻿using Evently.Common.Application.Messaging;
 using Evently.Common.Domain;
 using Evently.Modules.Users.Application.Abstractions.Data;
+using Evently.Modules.Users.Application.Abstractions.Identity;
 using Evently.Modules.Users.Domain.Users;
 
 namespace Evently.Modules.Users.Application.Users.RegisterUser;
 
 internal sealed class RegisterUserCommandHandler(
+    IIdentityProviderService identityProviderService,
     IUserRepository userRepository,
     IUnitOfWork unitOfWork
 )
@@ -13,7 +15,16 @@ internal sealed class RegisterUserCommandHandler(
 {
     public async Task<Result<Guid>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
     {
-        var user = User.Create(request.Email, request.FirstName, request.LastName);
+        Result<string> result = await identityProviderService.RegisterUserAsync(
+                new UserModel(request.Email, request.Password, request.FirstName, request.LastName),
+                cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return Result.Failure<Guid>(result.Error);
+        }
+
+        var user = User.Create(request.Email, request.FirstName, request.LastName, result.Value);
 
         userRepository.Insert(user);
 
